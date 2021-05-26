33737af5fc0a1fbf94acb6d57a0c1d9bbf2353a5
Hendrik Müller <henne90gen@gmail.com>
2021-05-26 13:22:13 +0200

    Removed old code

--- vr_env.cpp
+++ vr_env.cpp
@@ -65,14 +65,6 @@ void vr_env::draw(cgv::render::context &ctx) {
 		auto &clouds = ref_clouds_renderer(ctx);
 		clouds.set_render_style(clouds_style);
 		clouds.render(ctx);
-
-		//		auto &flat_color = ref_flat_color_renderer(ctx);
-		//		flat_color.set_render_style(flat_color_style);
-		//		flat_color.set_position_array(ctx, positions);
-		//		flat_color.set_texcoord_array(ctx, texcoords);
-		//		flat_color.set_texture(ctx, trees.get_tree_placement_texture());
-		//		flat_color.set_indices(ctx, indices);
-		//		flat_color.render(ctx, 0, indices.size());
 	});
 }
 
